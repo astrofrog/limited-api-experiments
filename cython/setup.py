7f1b6c4763ecdd7e50ebb2cd3cea90a9dbe9ffe0
Thomas Robitaille <thomas.robitaille@gmail.com>
2022-08-23 23:27:33 +0100

    Updates to setup.py

--- cython/setup.py
+++ cython/setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 import os
-import sys
 
 from setuptools import setup
 from setuptools.extension import Extension
 
+define_macros = [('CYTHON_LIMITED_API', '0x030800f0'),
+                 ('Py_LIMITED_API', '0x030800f0')]
+
 setup(ext_modules=[Extension("test_cython.example",
                              [os.path.join('test_cython', 'example.pyx')],
-                             py_limited_api=True)])
+                             py_limited_api=True,
+                             define_macros=define_macros)])
