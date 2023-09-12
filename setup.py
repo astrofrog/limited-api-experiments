7c59a9a1599f916c51edd87e350465b9aaa50ec1
Thomas Robitaille <thomas.robitaille@gmail.com>
2023-09-12 17:06:58 +0100

    cimport numpy example

--- setup.py
+++ setup.py
@@ -2,13 +2,24 @@
 
 import os
 
+import numpy as np
+
 from setuptools import setup
 from setuptools.extension import Extension
 
-define_macros = [('CYTHON_LIMITED_API', '1'),
-                 ('Py_LIMITED_API', '0x030700f0')]
+define_macros = [
+    ("CYTHON_LIMITED_API", "1"),
+    ("Py_LIMITED_API", "0x030700f0"),
+]
 
-setup(ext_modules=[Extension("test_cython.example",
-                             [os.path.join('test_cython', 'example.pyx')],
-                             py_limited_api=True,
-                             define_macros=define_macros)])
+setup(
+    ext_modules=[
+        Extension(
+            "test_cython.example",
+            [os.path.join("test_cython", "example.pyx")],
+            py_limited_api=True,
+            include_dirs=[np.get_include()],
+            define_macros=define_macros,
+        )
+    ]
+)
--- test_cython/example.pyx
+++ test_cython/example.pyx
@@ -1,9 +1,1 @@
-import cython
-
-@cython.binding(False)
-def fib(int n):
-    cdef int a, b
-    a, b = 0, 1
-    while b < n:
-        a, b = b, a + b
-    return b
+cimport numpy
